b5f6f355700c8733b30bf9bb03a7ed300f637d93
Cathy Liu <cathy30197@gmail.com>
2025-01-09 15:02:14 +0800

    Update project structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
-import HomepageComponent from './components/HomepageComponent';
+import Header from './components/Header/Header';
+import Homepage from './components/HomePage/Homepage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Router>
-        <HeaderComponent />
+        <Header />
         <div className="container" style={{marginTop: "20px"}}>
           <Routes> {/* http://localhost:3000/ */}
-            <Route path='/' exact element={<HomepageComponent />}></Route>
+            <Route path='/' exact element={<Homepage />}></Route>
           </Routes>
         </div>
-        <FooterComponent />
+        <Footer />
       </Router>
     </div>
   );
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function HeaderComponent() {
+export default function Header() {
   return (
     <div>
         <header>

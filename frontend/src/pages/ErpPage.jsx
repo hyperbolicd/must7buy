7d479beb652fe287e8fba46db1a3e2e435d1ac79
Cathy Liu <cathy30197@gmail.com>
2025-01-17 22:05:06 +0800

    Update MainPage and ErpPage structure

--- frontend/src/pages/ErpPage.jsx
+++ frontend/src/pages/ErpPage.jsx
@@ -1,39 +1,17 @@
-import React, { Fragment } from 'react'
-import { Link, Outlet } from 'react-router-dom'
-import Header from '../components/Header/Header'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer/Footer'
-import styles from './ErpPage.module.css'
+import Body from '../components/atoms/Body/Body'
+import ErpHeader from '../components/ErpHeader/ErpHeader'
 
 export default function ErpPage() {
-  const navs = [
-    { name: '商品管理', path: 'products', isOpen: true},
-    { name: '用戶管理', path: 'customers', isOpen: true},
-    { name: '訂單管理', path: 'orders', isOpen: false},
-    { name: '統計報表', path: 'dashboard', isOpen: false},
-    { name: '員工管理', path: 'employees', isOpen: true},
-    { name: 'Home', path: '', isOpen: true},
-    { name: '前台', path: '/', isOpen: true},
-  ]
-
+  
   return (
     <div>
-      <nav>
-        <div className={styles.function}>
-        <span><b>後臺管理系統</b></span> 
-        </div>
-        { 
-          navs.map(nav => {
-            return (
-              <div key={nav.path} className={styles.function}>
-                <Link to={nav.path} className={!nav.isOpen && styles.disabled} target={ nav.name === '前台' && '_blank'}>{nav.name}</Link>
-              </div>
-            )
-          })
-        }
-      </nav>
-        <div className={styles.container}>
-            <Outlet />
-        </div>
+      <ErpHeader />
+      <Body>
+          <Outlet />
+      </Body>
       <Footer />
     </div>
   )
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
-import styles from './MainPage.module.css'
+import Body from '../components/atoms/Body/Body'
 
 export default function MainPage() {
   return (
     <div>
-        <Header />
-            <div className={styles.container}>
-                <Outlet />
-            </div>
-        <Footer />
+      <Header />
+      <Body>
+          <Outlet />
+      </Body>
+      <Footer />
     </div>
   )
 }
